45aa055caf8c1636c0e78a5631c0233ceec7f5c6
DESKTOP-B09ANSL\Quang <chuvanquang0104@gmail.com>
2021-03-20 23:28:25 +0700

    Finish WelcomeScreen

--- lib/Screens/Welcome/Components/body.dart
+++ lib/Screens/Welcome/Components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:welcome_login_signup/components/rounded_button.dart';
+import 'package:welcome_login_signup/constants.dart';
 
 import 'background.dart';
 
@@ -8,20 +10,39 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "WELCOME TO EDU",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "WELCOME TO EDU",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          SvgPicture.asset(
-            "assets/icons/chat.svg",
-            height: size.height * 0.4,
-          )
-        ],
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            SvgPicture.asset(
+              "assets/icons/chat.svg",
+              height: size.height * 0.45,
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              color: kPrimaryColor,
+              press: () {},
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final Function press;
+  final Color color, textColor;
+
+  const RoundedButton({
+    Key key,
+    this.text,
+    this.press,
+    this.color = kPrimaryColor,
+    this.textColor = Colors.white,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      width: size.width * 0.6,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29.0),
+        child: TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(color),
+            padding: MaterialStateProperty.all(
+                EdgeInsets.symmetric(horizontal: 20, vertical: 20)),
+          ),
+          onPressed: press,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
